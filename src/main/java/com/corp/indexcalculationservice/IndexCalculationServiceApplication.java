a3cfebc62047eda5e4da434708792e391a521929
MIHIR KUMAR JHA <mihir.iiest@gmail.com>
2021-02-16 20:56:15 +0100

    Async changes

--- src/main/java/com/corp/indexcalculationservice/IndexCalculationServiceApplication.java
+++ src/main/java/com/corp/indexcalculationservice/IndexCalculationServiceApplication.java
@@ -1,13 +1,29 @@
 package com.corp.indexcalculationservice;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class IndexCalculationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IndexCalculationServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public Executor taskExecutor() {
+	    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+	    executor.setCorePoolSize(10);
+	    executor.setMaxPoolSize(10);
+	    executor.setQueueCapacity(500);
+	    executor.setThreadNamePrefix("Ticks-");
+	    executor.initialize();
+	    return executor;
+	}
 }
--- src/main/java/com/corp/indexcalculationservice/controller/IndexCalculationController.java
+++ src/main/java/com/corp/indexcalculationservice/controller/IndexCalculationController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +34,8 @@ public class IndexCalculationController {
 
 	@Autowired
 	private IndexCalculationService indexService;
-
+    
+	@Async
 	@ApiOperation("API to add new ticks")
 	@PostMapping("/ticks")
 	public void addTicks(@RequestBody @Validated AddTicksRequest addTicksRequest,HttpServletResponse response) {
